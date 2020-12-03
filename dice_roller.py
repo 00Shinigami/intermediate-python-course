cd292d10e50678ce34f81d50cb8d4d53938c61f2
00Shinigami <mail@max-ramacher.de>
2020-12-03 20:33:50 +0100

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,9 +1,17 @@
+import random
 import time
 from time import sleep
 
+dice_rolls = 2
+dice_sum = 0
+roll = random.randint(1,6)
+
 def main():
-  print('You rolled a die')
-  print("by Max")
+  for i in range(0,dice_rolls):
+   dice_sum += roll
+  print(f'You rolled a {roll}')
+print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
+  time.sleep(2)
